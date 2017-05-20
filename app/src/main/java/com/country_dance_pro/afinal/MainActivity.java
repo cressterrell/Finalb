7e5972db57ac060f7f2634948a99f0a703f0a7d5
cressterrell <cressterrell@gmail.com>
2017-05-19 20:49:29 -0500

    Initial commit here

---
+++ app/src/main/java/com/country_dance_pro/afinal/MainActivity.java
@@ -0,0 +1,99 @@
+package com.country_dance_pro.afinal;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void play1(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://youtu.be/qa_-gZtwCZ4"));
+                startActivity(browserIntent);
+    }
+    public void play2(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play3(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play4(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play5(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play6(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play7(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play8(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play9(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play10(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play11(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play12(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play13(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play14(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play15(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play16(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play17(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play18(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play19(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    public void play20(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+}
+
